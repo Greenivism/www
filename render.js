f50f963394f5b06261446463ad032e5a5d64354b
OpenShift guest <matt@greenivism.org>
2013-01-20 23:23:19 -0500

    asdfsadf

---
+++ render.js
@@ -0,0 +1,7 @@
+$('.header').append('<a href="http://www.greenivism.org" class="title">Greenivism</a>');
+$('.header').append('<a href="#" class="nav-link">Raise Awareness</a>');
+$('.header').append('<a href="#" class="nav-link">Get Involved</a>');
+$('.header').append('<a href="#" class="nav-link">Greenivist tips</a>');
+$('.header').append('<a href="donate.html" class="nav-link">Donate</a>');
+
+$('.footer').append('<a href="#" class="footerlink">2013 Greenivism Corp.</a>');
