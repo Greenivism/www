3b7243198335a306ae9696dfe10fdc5dd8587a4e
OpenShift guest <matt@greenivism.org>
2013-01-22 23:44:02 -0500

    manifesto time

--- render.js
+++ render.js
@@ -1,7 +1,7 @@
 $('.header').append('<a href="index.html" class="title">Greenivism</a>');
 $('.header').append('<a href="spreadtheword.html" class="nav-link">Spread the word</a>');
 $('.header').append('<a href="getinvolved.html" class="nav-link">Get Involved</a>');
-$('.header').append('<a href="tips.html" class="nav-link">Greenivist tips</a>');
+$('.header').append('<a href="greenivistmanifesto.html" class="nav-link">Greenivist Manifesto</a>');
 $('.header').append('<a href="donate.html" class="nav-link">Donate</a>');
 
 $('.footer').append('<a href="#" class="footerlink">2013 Greenivism Corp.</a>');
