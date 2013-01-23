8fd431020e6c367f734e91d9135383d7c0463273
OpenShift guest <matt@greenivism.org>
2013-01-23 01:13:45 -0500

    fixed header?

--- render.js
+++ render.js
@@ -1,8 +1,8 @@
 $('.header').append('<a href="index.html" class="title">Greenivism</a>');
-$('.header').append('<a href="spreadtheword.html" class="nav-link">Spread the word</a>');
-$('.header').append('<a href="getinvolved.html" class="nav-link">Get Involved</a>');
-$('.header').append('<a href="greenivistmanifesto.html" class="nav-link">Greenivist Manifesto</a>');
-$('.header').append('<a href="donate.html" class="nav-link">Donate</a>');
+$('.header').append('<a href="spreadtheword.html" class="header-link">Spread the word</a>');
+$('.header').append('<a href="getinvolved.html" class="header-link">Get Involved</a>');
+$('.header').append('<a href="greenivistmanifesto.html" class="header-link">Greenivist Manifesto</a>');
+$('.header').append('<a href="donate.html" class="header-link">Donate</a>');
 
 $('.footer').append('<a href="#" class="footerlink">2013 Greenivism Corp.</a>');
 
